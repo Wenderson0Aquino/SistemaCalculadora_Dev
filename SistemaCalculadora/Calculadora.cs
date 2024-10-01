12181e90465dbf3c62c978f72d9d0524bd2ea59d
Wenderson Aquino <wensantos.aquino@gmail.com>
2024-09-30 21:31:44 -0300

    Add files via upload

---
+++ SistemaCalculadora/Calculadora.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace SistemaCalculadora
+{
+    internal class Calculadora
+    {
+        private double PrimeiroNumero { get; set; }
+        private double SegundoNumero { get; set; }
+
+        public Calculadora() { }
+
+        public double Somar(double primeiroNumero, double segundoNumero)
+        {
+            double resultado = primeiroNumero + segundoNumero;
+            return resultado;
+        }
+        public double Subtrair(double primeiroNumero, double segundoNumero)
+        {
+            double resultado = primeiroNumero - segundoNumero;
+            return resultado;
+        }
+        public double Multiplicar(double primeiroNumero, double segundoNumero)
+        {
+            double resultado = primeiroNumero * segundoNumero;
+            return resultado;
+        }
+        public double Dividir(double primeiroNumero, double segundoNumero)
+        {
+            double resultado = primeiroNumero / segundoNumero;
+            return resultado;
+        }
+        public double CalcularPotencia(double primeiroNumero, double segundoNumero)
+        {
+            double resultado = Math.Pow(primeiroNumero, segundoNumero);
+            return resultado;
+        }
+        public static string Baskara(double a, double b, double c)
+        {
+            double delta = b * b - (4 * a * c);
+
+            if (delta > 0)
+            {
+                double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+
+                return "X¹: " + x1.ToString() + "\nX²: " + x2.ToString();
+            }
+            else
+            {
+                if (delta == 0)
+                {
+                    double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                    return "X¹ e X²: " + x1.ToString();
+                }
+                else
+                {
+                    return "Não existem raizer reais!";
+                }
+            }
+        }
+        ~Calculadora() { }
+    }
+}
---
+++ SistemaCalculadora/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaCalculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora calculadora = new Calculadora();   
+            Console.WriteLine(Calculadora.Baskara(1,4,5));
+            Console.ReadKey();
+        }
+    }
+}
